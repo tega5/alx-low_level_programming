c4bb89d4f0e9dda17479f2534ee7cd337a34bba7
Oghenetega Christabella Efe-Makpamekun <oghenetegaefe.makpamekun5@gmail.com>
2023-06-15 04:35:04 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 
 /**
  *main - Entry point
- *Return 1 (Success) 
+ *Return: Always 1
  */
 
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
