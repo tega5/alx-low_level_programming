741eb64917ee15eaf06011330a1b0ce54178e85e
Oghenetega Christabella Efe-Makpamekun <oghenetegaefe.makpamekun5@gmail.com>
2023-06-21 07:10:22 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - fibonacci <3
+ *
+ * Purpose - no hardcode
+ *
+ * Return: (Success)
+ */
+
+int main(void)
+{
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int 1 = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1
