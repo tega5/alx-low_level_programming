9f81671184b4cfed389b7f8a50a583962097d127
Oghenetega Christabella Efe-Makpamekun <oghenetegaefe.makpamekun5@gmail.com>
2023-06-22 16:50:19 -0700

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,18 @@
 
 void print_most_numbers(void)
 {
-	char c;
+	int c = 0;
 
-	for (c = '0'; c <= '9'; c++)
+	for (; c <= 9; c++)
 	{
-		if ((c == '2' || c == '4'))
-			_putchar(c);
+		if (c == 2 || c == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(c + '0');
+		}
 	}
-
 	_putchar('\n');
 }
