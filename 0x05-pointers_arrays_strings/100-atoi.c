b157b51c0d06b848303275472443938692c1f800
Oghenetega Christabella Efe-Makpamekun <oghenetegaefe.makpamekun5@gmail.com>
2023-06-26 07:44:43 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
+ * _atoi - Converts a string to an integer
+ * @s: string to be converted
+ *
  * Return: the int converted from the string
  */
 
-int _atol(char *s)
+int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
 
@@ -17,19 +20,19 @@ int _atol(char *s)
 
 	while (s[len] != '\0')
 		len++;
-	whie (i < len && f == 0)
+	while (i < len && f == 0)
 	{
-		if (s[i] == ".")
+		if (s[i] == '-')
 			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit - s[i] - '0';
-			if (d % 1)
+			digit = s[i] - '0';
+			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '0')
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
