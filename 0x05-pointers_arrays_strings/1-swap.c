2c99864527149fcd0414f5e6d37aa01afd93e69a
Oghenetega Christabella Efe-Makpamekun <oghenetegaefe.makpamekun5@gmail.com>
2023-06-26 07:53:51 -0700

    1-swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * swap_int - Swaps the value of two integers
+ * swap_int - Swaps the values of two integers
  * @a: Integer to swap
  * @b: Integer to swap
  */
@@ -10,7 +10,7 @@ void swap_int(int *a, int *b)
 {
 	int m;
 
-	m = *a
-	*a = *b
+	m = *a;
+	*a = *b;
 	*b = m;
 }
