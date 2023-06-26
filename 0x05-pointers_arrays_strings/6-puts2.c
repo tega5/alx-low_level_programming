e82b81cb1d619b684560b557080b570e9437ecfb
Oghenetega Christabella Efe-Makpamekun <oghenetegaefe.makpamekun5@gmail.com>
2023-06-26 08:26:06 -0700

    3-puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,30 +2,23 @@
 
 /**
  * puts2 - Function should print only one charcater out of two
- * Starting with the first one
+ * starting with the first one
  * @str: Input
  * Return: print
  */
 
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int i = 0;
 
-	while (*y != '\0')
+	while (str[i] != '\0')
 	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0; o <= t; o++)
-	{
-		if (o % 2 == 0)
-	{
-		_putchar(str[o]);
-	}
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+
+		i++
 	}
 	_putchar('\n');
 }
