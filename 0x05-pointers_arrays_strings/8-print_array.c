e506955aa28fe008b18fc4dcd747a8f689a90c4a
Oghenetega Christabella Efe-Makpamekun <oghenetegaefe.makpamekun5@gmail.com>
2023-06-26 07:34:21 -0700

    8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,7 @@
 /**
  * print_array - A function that prints n elements of an array
  * @a: Array name
- * @n: Is the number of elements of the array to be printed
+ * @n: Is the number of elements OF the array to be printed
  * Return: a and n inpits
  */
 
@@ -13,11 +13,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < (n - 1); i++)
 	{
-		print("%d, ", a[i]);
+		printf("%d, ", a[i]);
 	}
 		if (i == (n - 1))
 		{
 			printf("%d", a[n - 1]);
 		}
-		printf("\n"):
+		printf("\n");
 }
